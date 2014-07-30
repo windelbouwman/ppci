f06842f9b2f14057d8b1efc4dd27ce31c438920c
Windel Bouwman <none@none>
2014-07-30 19:40:33 +0200

    Move tests outside module

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ppci',
     version='0.0.1',
     author='Windel Bouwman',
-    packages=find_packages(),
+    packages=find_packages(exclude=["*.test.*"]),
     url='https://bitbucket.org/windel/ppci',
     license='LICENSE.txt',
 )
