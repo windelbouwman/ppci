b8575882596ac770113d6b96c062f861c3fdd69f
Windel Bouwman <none@none>
2014-08-15 11:32:59 +0200

    Added description for flashing of stm32f4xx

--- bin/st-flash.py
+++ bin/st-flash.py
@@ -5,18 +5,20 @@ import sys
 from ppci.utils import stlink, stm32
 from ppci.utils import hexfile
 
+
 def hex2int(s):
-   if s.startswith('0x'):
-      s = s[2:]
-      return int(s, 16)
-   raise ValueError('Hexadecimal value must begin with 0x')
+    if s.startswith('0x'):
+        s = s[2:]
+        return int(s, 16)
+    raise ValueError('Hexadecimal value must begin with 0x')
 
 
 def make_parser():
     parser = argparse.ArgumentParser(
-       description='ST-link flash utility by Windel Bouwman')
-    subparsers = parser.add_subparsers(title='commands', 
-             description='possible commands', dest='command')
+        description='ST-link flash utility by Windel Bouwman')
+    subparsers = parser.add_subparsers(
+        title='commands',
+        description='possible commands', dest='command')
 
     readparser = subparsers.add_parser('read', help='read flash contents')
     readparser.add_argument('filename', type=argparse.FileType('wb', 0))
@@ -27,61 +29,67 @@ def make_parser():
     writeparser.add_argument('filename', type=argparse.FileType('rb'))
     writeparser.add_argument('address', type=hex2int)
 
-    hexwriteparser = subparsers.add_parser('hexwrite', help='write hexfile to flash')
+    hexwriteparser = subparsers.add_parser(
+        'hexwrite', help='write hexfile to flash')
     hexwriteparser.add_argument('hexfile', type=argparse.FileType('r'))
 
-    verifyparser = subparsers.add_parser('verify', help='verify flash contents')
+    verifyparser = subparsers.add_parser('verify',
+                                         help='verify flash contents')
     verifyparser.add_argument('filename', type=argparse.FileType('rb'))
     verifyparser.add_argument('address', type=hex2int)
 
-    eraseparser = subparsers.add_parser('erase', help='erase flash contents')
+    subparsers.add_parser('erase', help='erase flash contents')
     return parser
 
 
-args = parser.parse_args()
-if not args.command:
-   parser.print_usage()
-   sys.exit(1)
+def do_flashing(args):
+    # In any command case, open a device:
+    stl = stlink.STLink2()
+    stl.open()
 
-# In any command case, open a device:
-stl = stlink.STLink2()
-stl.open()
+    # Enter the right mode:
+    if stl.CurrentMode == stlink.DFU_MODE:
+        stl.exitDfuMode()
 
-# Enter the right mode:
-if stl.CurrentMode == stlink.DFU_MODE:
-   stl.exitDfuMode()
+    if stl.CurrentMode != stlink.DEBUG_MODE:
+        stl.enterSwdMode()
 
-if stl.CurrentMode != stlink.DEBUG_MODE:
-   stl.enterSwdMode()
+    if stl.ChipId != 0x10016413:
+        print('Only working on stm32f4discovery board for now.')
+        sys.exit(2)
 
-if stl.ChipId != 0x10016413:
-   print('Only working on stm32f4discovery board for now.')
-   sys.exit(2)
+    # Retrieve the connected device, if any:
+    dev = stl.createDevice()
 
-# Retrieve the connected device, if any:
-dev = stl.createDevice()
+    if args.command == 'read':
+        dev_content = dev.readFlash(args.address, args.size)
+        args.filename.write(dev_content)
+    elif args.command == 'write':
+        content = args.filename.read()
+        dev.writeFlash(args.address, content)
+    elif args.command == 'hexwrite':
+        hf = hexfile.HexFile()
+        hf.load(args.hexfile)
+        for region in hf.regions:
+            print('flashing {}'.format(region))
+            dev.writeFlash(region.address, region.data)
+    elif args.command == 'verify':
+        content = args.filename.read()
+        dev.verifyFlash(args.address, content)
+    elif args.command == 'erase':
+        dev.eraseFlash()
+    else:
+        print('unknown command', args.command)
 
-if args.command == 'read':
-   dev_content = dev.readFlash(args.address, args.size)
-   args.filename.write(dev_content)
-elif args.command == 'write':
-   content = args.filename.read()
-   dev.writeFlash(args.address, content)
-elif args.command == 'hexwrite':
-    hf = hexfile.HexFile()
-    hf.load(args.hexfile)
-    for region in hf.regions:
-        print('flashing {}'.format(region))
-        dev.writeFlash(region.address, region.data)
-elif args.command == 'verify':
-   content = args.filename.read()
-   dev.verifyFlash(args.address, content)
-elif args.command == 'erase':
-   dev.eraseFlash()
-else:
-   print('unknown command', args.command)
+    stl.reset()
+    stl.run()
+    stl.exitDebugMode()
 
-stl.reset()
-stl.run()
-stl.exitDebugMode()
 
+if __name__ == '__main__':
+    parser = make_parser()
+    args = parser.parse_args()
+    if not args.command:
+        parser.print_usage()
+        sys.exit(1)
+    do_flashing(args)
--- docs/stlink.rst
+++ docs/stlink.rst
@@ -5,3 +5,32 @@ St link
 
 Programming the stm32f4 discovery board.
 
+Building
+========
+
+Compiling the stm32f4xx example project using the compiler toolchain:
+
+.. code:: bash
+
+    $ cd test/data/stm32f4xx
+    $ zcc.py
+    2014-08-15 11:27:48,844|INFO|taskrunner|Target sequence: [Target "burn2", Target "all"]
+    2014-08-15 11:27:48,844|INFO|taskrunner|Target burn2
+    2014-08-15 11:27:48,845|INFO|taskrunner|Running Task "AssembleTask"
+    2014-08-15 11:27:48,845|INFO|taskrunner|Running Task "C3cTask"
+    2014-08-15 11:27:48,909|INFO|taskrunner|Running Task "LinkTask"
+    2014-08-15 11:27:48,915|INFO|taskrunner|Target all
+    2014-08-15 11:27:48,915|INFO|taskrunner|Done!
+    $ ls
+
+
+Flashing
+========
+
+To flash an application using the st-link V2 debugger, use the following
+command:
+
+.. code:: bash
+
+    $ st-flash.py hexwrite burn2.hex
+
