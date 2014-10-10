ad5c2ca23e36c0fdff66efd4d688fe7ba2a49a96
Windel Bouwman <none@none>
2014-10-10 11:38:52 +0200

    add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('readme.rst') as f:
+    long_description = f.read()
+
 setup(
     name='ppci',
     description="Pure python compiler infrastructure",
     scripts=["bin/zcc.py", "bin/st-flash.py", 'bin/hexutil.py'],
+    long_description=long_description,
     version='0.0.1',
     author='Windel Bouwman',
     include_package_data=True,
