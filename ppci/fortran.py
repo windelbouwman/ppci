7289fb0298dfcc6ff6ac3b5e8b51c4436613f599
Windel Bouwman <none@none>
2016-02-13 13:06:58 +0100

    Add badges and donate button to website

--- ppci/fortran.py
+++ ppci/fortran.py
@@ -1,8 +1,72 @@
 
+from .pcc.baselex import BaseLexer
+from .common import Token, SourceLocation
+
 """
 https://en.wikibooks.org/wiki/Fortran_77_Tutorial#Why_learn_Fortran.3F
 """
 
+
+class FortranLexer(BaseLexer):
+    """
+        Handle the nice fortran syntax:
+        column use
+        1-5    label
+        6      continuation character
+        7-72   statements
+        73-80  unused
+    """
+    def __init__(self):
+        op_txt = r'[,=+*/]'
+        tok_spec = [
+            ('REAL', r'\d+\.\d+', lambda typ, val: (typ, float(val))),
+            ('NUMBER', r'\d+', lambda typ, val: (typ, int(val))),
+            ('ID', r'[A-Za-z][A-Za-z\d_]*', self.handle_id),
+            ('SKIP', r'[ \t]', None),
+            ('LEESTEKEN', op_txt, lambda typ, val: (val, val)),
+            ('STRING1', r'"[^"]*"', lambda typ, val: ('STRING', val[1:-1])),
+            ('STRING2', r"'[^']*'", lambda typ, val: ('STRING', val[1:-1]))
+            ]
+        super().__init__(tok_spec)
+
+    def handle_id(self, typ, val):
+        return (typ, val)
+
+    def tokenize(self, src):
+        loc = SourceLocation('a.txt', 1, 1, 1)
+        for line in src.split('\n'):
+            line = line.rstrip()
+            if not line:
+                continue
+            if line[0] == 'C':
+                continue
+            label, cont, statements = self.split_line(line)
+            if label:
+                yield Token('LABEL', label, loc)
+            if statements:
+                for token in super().tokenize(statements):
+                    yield token
+
+    def split_line(self, line):
+        """ Split line depending on column """
+        def int2(l):
+            l = l.strip()
+            if l:
+                return int(l)
+        if len(line) < 6:
+            return int2(line), None, None
+        elif len(line) < 7:
+            return int2(line[0:5]), line[5] != ' ', None
+        else:
+            return int2(line[0:5]), line[5] != ' ', line[6:]
+
+
 class FortranParser:
+    def __init__(self):
+        self.lexer = FortranLexer()
+
     def parse(self, src):
-        pass
+        """ parse a piece of FORTRAN77 """
+        for token in self.lexer.tokenize(src):
+            print(token)
+        print('===========')
--- test/test_fortran.py
+++ test/test_fortran.py
@@ -1,22 +1,41 @@
 import unittest
 from ppci.fortran import FortranParser
 
+example = """
+C234567890
+      PROGRAM PETROL
+      INTEGER STOPS, FILLUP
+C
+C THESE VARIABLES WOULD OTHERWISE BE TYPED REAL BY DEFAULT
+C ANY TYPE SPECIFICATIONS MUST PRECEDE THE FIRST EXECUTABLE STATEMENT
+C
+      READ *, KM,STOPS,FILLUP
+      USED = 40*STOPS + FILLUP
+C COMPUTES THE PETROL USED AND CONVERTS IT TO REAL
+      KPL = KM/USED + 0.5
+C 0.5 IS ADDED TO ENSURE THAT THE RESULT IS ROUNDED
+      PRINT *, 'AVERAGE KPL WAS',KPL
+      END
+"""
+
+hello_world_src = """
+      PRINT *, "Hello World!"
+      END
+"""
+
 
 class FortranTestCase(unittest.TestCase):
     def setUp(self):
         self.parser = FortranParser()
 
     def do(self, src):
-        print(src)
         self.parser.parse(src)
 
     def test_hello_world(self):
-        src = """
-        PRINT *, "Hello World!"
-               END
-        """
-        self.do(src)
+        self.do(hello_world_src)
 
+    def test_example(self):
+        self.do(example)
 
 if __name__ == '__main__':
     unittest.main()
