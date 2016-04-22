3deab934d2de619d22f4f0a0a4813a32f0379c75
michg <none@none>
2016-04-22 21:22:46 +0200

    riscv pulpino example: usage of ppci.binutils.dbg api in dbg.py

--- examples/riscvpulpino/mkfirmpulpino.py
+++ examples/riscvpulpino/mkfirmpulpino.py
@@ -5,9 +5,12 @@ from ppci.utils.reporting import HtmlReportGenerator, complete_report
 report_generator = HtmlReportGenerator(open("report.html", 'w'))
 with complete_report(report_generator) as reporter:
     
-    o1 = asm ("startercode.asm", "riscv")
-    o2 = c3c(["bsp.c3", "io.c3", "main.c3"], [], "riscv",reporter=reporter)
-    obj = link([o1,o2], "firmware.mmap", "riscv", use_runtime=False,reporter=reporter)
+    o1 = asm("startercode.asm", "riscv")
+    o2 = c3c(["bsp.c3", "io.c3", "main.c3"], [], "riscv", reporter=reporter, debug=True)
+    obj = link([o1,o2], "firmware.mmap", "riscv", use_runtime=False,reporter=reporter, debug=True)
+    o1f = open("samples.txt","w")
+    obj.save(o1f)
+    o1f.close()
     objcopy(obj, "flash", "elf", "samples.elf")
     objcopy(obj, "flash", "bin", "flash.bin")
     objcopy(obj, "flash", "ldb", "debug.txt")
--- ppci/arch/riscv/arch.py
+++ ppci/arch/riscv/arch.py
@@ -7,7 +7,7 @@ from ..arch import Architecture, Label, VCall
 from .instructions import isa, Mov2
 from .rvc_instructions import rvcisa
 from .registers import RiscvRegister
-from .registers import R0, LR, SP, R3, R4, R5, R6, R7, FP, R10, R11, R12
+from .registers import R0, LR, SP, R3, R4, R5, R6, R7, FP, R10, R11, R12, all_registers
 from .registers import R13, R14, R15, R16, R17, R28, LR
 from ...ir import i8, i32, ptr
 from ..data_instructions import data_isa
@@ -51,6 +51,7 @@ class RiscvArch(Architecture):
             self.isa = isa + rvcisa + data_isa
         else:
             self.isa = isa + data_isa
+        self.registers.extend(all_registers)
         self.FrameClass = RiscvFrame
         self.assembler = RiscvAssembler()
         self.assembler.gen_asm_parser(self.isa)
