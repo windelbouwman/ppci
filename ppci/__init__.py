97c7a11a8bfefb451331addb37e63d9330673ebb
Windel Bouwman <none@none>
2016-05-06 21:33:46 +0200

    Remove usage of tox from setup.py and use unittest builtin library

--- ppci/__init__.py
+++ ppci/__init__.py
@@ -5,8 +5,7 @@
 import sys
 
 # Define version here. Used in docs, and setup script:
-version = '0.5.0'
+version = '0.5'
 
 # Assert python version:
 assert sys.version_info.major == 3, "Needs to be run in python version 3.x"
-
--- setup.py
+++ setup.py
@@ -1,34 +1,18 @@
+import unittest
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-import sys
 import ppci
 
+print(ppci)
 
 with open('readme.rst') as f:
     long_description = f.read()
 
 
-class Tox(TestCommand):
-    user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.tox_args = None
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import tox
-        import shlex
-        args = self.tox_args
-        if args:
-            args = shlex.split(self.tox_args)
-        errno = tox.cmdline(args=args)
-        sys.exit(errno)
+def ppci_test_suite():
+    """ Load the ppci test suite """
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover('test', pattern='test*.py')
+    return test_suite
 
 
 setup(
@@ -53,9 +37,7 @@ setup(
     package_data={'': ['*.grammar', "*.sled", "*.rst"]},
     url='https://ppci.readthedocs.io/',
     license='BSD',
-    test_suite="test",
-    tests_require=['tox'],
-    cmdclass={'test': Tox},
+    test_suite="setup.ppci_test_suite",
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Development Status :: 3 - Alpha',
