2eafc388b6997f1c44b773ab62e2e407521695b3
Windel Bouwman <none@none>
2016-05-08 18:32:46 +0200

    Fix docs build

--- docs/exts/zipexamples.py
+++ docs/exts/zipexamples.py
@@ -1,23 +1,32 @@
 # Custom extension that zips all the examples for download.
 from zipfile import ZipFile
-from os import path
-import glob
+import fnmatch
+from os import path, walk
+
+
+my_path = path.dirname(path.abspath(__file__))
+root_path = path.abspath(path.join(my_path, '..', '..'))
+zip_filename = path.join(root_path, 'docs', 'examples.zip')
+
+
+def my_glob(folder, ext):
+    """ glob does not work recursively in all python versions """
+    for root, dirnames, filenames in walk(folder):
+        for filename in fnmatch.filter(filenames, ext):
+            yield path.join(root, filename)
 
 
 def zip_examples(app):
     glob_patterns = [
-        "examples/**/*.c3",
-        "examples/**/*.asm",
-        "examples/**/*.mmap",
-        "examples/**/build.xml",
-        "librt/*.c3"]
-    my_path = path.dirname(path.abspath(__file__))
-    root_path = path.abspath(path.join(my_path, '..', '..'))
-    zip_filename = path.join(root_path, 'docs', 'examples.zip')
+        ("examples", "*.c3"),
+        ("examples", "*.asm"),
+        ("examples", "*.mmap"),
+        ("examples", "build.xml"),
+        ("librt", "*.c3")]
     with ZipFile(zip_filename, 'w') as myzip:
-        for glob_pattern in glob_patterns:
-            pat = path.join(path.join(root_path, glob_pattern))
-            for filename in glob.iglob(pat, recursive=True):
+        for folder, ext in glob_patterns:
+            pat = path.join(path.join(root_path, folder))
+            for filename in my_glob(pat, ext):
                 zfn = path.relpath(filename, root_path)
                 app.info('zipping {} as {}'.format(filename, zfn))
                 myzip.write(filename, zfn)
