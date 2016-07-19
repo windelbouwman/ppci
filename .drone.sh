c73d165cc16d5899e68e2aa2cb6a92ba29185d6c
Windel Bouwman <none@none>
2016-07-19 13:08:54 +0200

    More artifacts

---
+++ .drone.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is the script run on the drone.io CI service.
+
+pip install tox codecov wheel --use-mirrors
+
+# Run tox:
+tox --version
+tox -e py33,cover
+codecov >/dev/null
+
+# Run setup script:
+python setup.py sdist bdist_wheel
