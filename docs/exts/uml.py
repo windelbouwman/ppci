4649ee79c897235b32621b11eebf5ad70ac426dd
Windel Bouwman <windel@windel.nl>
2018-02-28 16:56:54 +0100

    Fix uml plugin

--- docs/exts/uml.py
+++ docs/exts/uml.py
@@ -1,7 +1,7 @@
 import tempfile
 import subprocess
 import os
-from sphinx.util.compat import Directive
+from docutils.parsers.rst import Directive
 from sphinx.ext.graphviz import graphviz
 
 import pylint  # ensure that pyreverse is available
