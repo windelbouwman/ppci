0757c3991ebc04a8b30b56c54d5c7c5c6f867aa1
Windel Bouwman <windel@windel.nl>
2018-06-17 11:35:09 +0200

    Added forgotten digraph.py file

---
+++ ppci/graph/digraph.py
@@ -0,0 +1,62 @@
+""" Directed graph.
+
+In a directed graph, the edges have a direction.
+"""
+
+from collections import defaultdict
+from .graph import Graph, Node
+
+
+class DiGraph(Graph):
+    """ Directed graph. """
+    def __init__(self):
+        super().__init__()
+        self.suc_map = defaultdict(set)
+        self.pre_map = defaultdict(set)
+
+    def add_edge(self, n, m):
+        """ Add a directed edge from n to m """
+        assert n in self.nodes
+        assert m in self.nodes
+        if (n, m) not in self.edges:
+            self.edges.add((n, m))
+            self.suc_map[n].add(m)
+            self.pre_map[m].add(n)
+            self.adj_map[n].add(m)
+            self.adj_map[m].add(n)
+            self.degree_map[m] += 1
+            self.degree_map[n] += 1
+
+    def successors(self, n):
+        """ Get the successors of the node """
+        return self.suc_map[n] & self.nodes
+
+    def predecessors(self, n):
+        """ Get the predecessors of the node """
+        return self.pre_map[n] & self.nodes
+
+
+class DiNode(Node):
+    """ Node in a directed graph """
+    @property
+    def successors(self):
+        """ Get the successors of this node """
+        return self.graph.successors(self)
+
+    @property
+    def predecessors(self):
+        """ Get the predecessors of this node """
+        return self.graph.predecessors(self)
+
+
+def dfs(start_node):
+    """ Visit nodes in depth-first-search order """
+    visited = set()
+    worklist = [(None, start_node)]
+    while worklist:
+        parent, node = worklist.pop()
+        if node not in visited:
+            visited.add(node)
+            yield parent, node
+            for successor in node.successors:
+                worklist.append((node, successor))
