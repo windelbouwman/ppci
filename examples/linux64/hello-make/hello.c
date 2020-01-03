629ff58b8381db0fe00c3a812fad7b7e110f4228
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2020-01-03 18:09:47 +0300

    examples/linux64/hello-make: Example using single .c file and Makefile
    And a linker script. Made possible by the inline assembly support, added
    recently.
    
    The sample still has to perform various workarounds of PPCI warts, which
    rather be fixed before merging this sample.

---
+++ examples/linux64/hello-make/hello.c
@@ -0,0 +1,45 @@
+void puts(char *s);
+void putc(char c);
+void exit(int status);
+void syscall(int nr, int a, int b, int c);
+
+int main()
+{
+    puts("Hello, World!");
+    /* As we don't have any C startup code in this example, and main is
+       the executable entrypoint, we can't just return, but have to
+       call exit syscall explicitly. */
+    exit(0);
+}
+
+void puts(char *s)
+{
+    while (*s) {
+        putc(*s++);
+    }
+    putc('\n');
+}
+
+void putc(char c)
+{
+    syscall(1, 1, (int)&c, 1);
+}
+
+void exit(int status)
+{
+    syscall(60, status, 0, 0);
+}
+
+void syscall(int nr, int a, int b, int c)
+{
+    asm(
+        "mov rax, %0 \n"
+        "mov rdi, %1 \n"
+        "mov rsi, %2 \n"
+        "mov rdx, %3 \n"
+        "syscall \n"
+        :
+        : "r" (nr), "r" (a), "r" (b), "r" (c) // input registers, patched into %0 .. %3
+        : "rax", "rdi", "rsi", "rdx"  // clobber registers, handled by the register allocator
+    );
+}
